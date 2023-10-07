945c56f681c16f9925338a08e0bb61ada0efcab2
amritav <amritavisalam@gmail.com>
2023-10-07 16:34:09 -0400

    Base for contactus page and events page

--- frontend/src/components/Router.js
+++ frontend/src/components/Router.js
@@ -2,6 +2,8 @@ import React, { useEffect, useReducer, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
+import ContactUs from './ContactUs';
+import Events from './Events';
 
 function Router() {
   return (
@@ -10,8 +12,14 @@ function Router() {
         <Route path="/signup">
           <SignUp></SignUp>
         </Route>
-        <Route path="*">
+        <Route path="/signin">
           <SignIn></SignIn>
+        </Route>
+        <Route path="/contactus">
+          <ContactUs></ContactUs>
+        </Route>
+        <Route path="*">
+          <Events></Events>
         </Route>
       </Switch>
     </>
